18fb48bd0efc1e82c00a8c310f93ad3e33a975a9
BonjourGit <sam.fang@nokia-sbell.com>
2020-12-29 11:20:59 +0800

    update script

--- cmdgen.sh
+++ cmdgen.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# Argument is one-liner "Id<space>Name" which is the output of
+# './safaribooks.py --getplaylists' with some pre-processing
+# Id: a hexadecimal playlist id string assigned by oreilly.com
+# Name: playlist name set by user, can contain spaces
 playlist=$1
-# input is in format "id name" where name can contain spaces
+
 # replace first space with underscore
 playlistInfo=${playlist/ /_}
+
 playlistId=`echo ${playlistInfo} | cut -d '_' -f1`
 playlistName_=`echo ${playlistInfo} | cut -d '_' -f2`
-# replace all space with underscore for playlist name
+
+# replace all space with underscore within playlist name
 playlistName=${playlistName_// /_}
-echo "nohup ./safaribooks.py --book-info-retry 10 --html-retry 10 --playlist ${playlistId} >> logbook.${playlistName} 2>&1"
+
+# Create a command line that downloads the playlist as a background job
+# Copy & paste to use
+echo "nohup ./safaribooks.py --book-info-retry 10 --html-retry 10 --playlist ${playlistId} >> logbook.${playlistName} 2>&1 &"
+
+# Calling this script for all playlists
+# ./safaribooks.py --getplaylists | grep Playlist | cut -d ":" -f2 | xargs -d '\n' -I '{}' ./cmdgen.sh {}
+# append '> filename' to redirect the output to a file
+
