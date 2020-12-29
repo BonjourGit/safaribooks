ee84fcc124b8390cfb6949bba926aebc8a62eda9
BonjourGit <sam.fang@nokia-sbell.com>
2020-12-29 10:43:47 +0800

    update verify

--- verify.sh
+++ verify.sh
@@ -2,9 +2,12 @@
 
 # Examples:
 #
-# verbose
+# verbose (print book info)
 # ./verify.sh <playlist id> --verbose
 #
-# concise
+# concise (summary only)
 # ./verify.sh <playlist id>
+#
+# To verify all playlists:
+# ./safaribooks.py --getplaylists | grep Playlist | cut -d ":" -f2 | xargs -d '\n' -I '{}' ./verify.sh {}
 ./safaribooks.py --book-info-retry 10 --playlist $@ --verify
